1c829c873901c1edb146c3649668fc7591dce9d6
ketamine <vladg3@mail.ru>
2023-10-15 15:31:12 +0700

    Добавил прогресс закгрузки экрана авторизации с помощью KVO

---
+++ Image Feed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "PGHzLzo86VTghjj6nxp0ZpwQ6_90yUdvvN2GNE0fUxY"
+let SecretKey = "rnjPA7jMq1-FBRKrKwmsbkWAXP-IuKuBcGnMl7W_VRY"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
